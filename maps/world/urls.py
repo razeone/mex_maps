d34e5e2ac2b219fd24a68ecc4272b0c7f405f410
Jorge Raze <jorge.raze@clevertech.biz>
2016-11-20 15:37:41 -0600

    Fixing an API, test is needed

--- maps/world/urls.py
+++ maps/world/urls.py
@@ -1,22 +1,23 @@
 from django.conf.urls import url
+from django.conf.urls import include
 
-from rest_framework.urlpatterns import format_suffix_patterns
+# We need to check that gives us the format_suffix_patterns
+# from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import routers
 
 from .views import CountryViewSet
 from .views import GetGeoJSONByCountry
 from .views import GetCentroidByCountry
 
-country_list = CountryViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
+router = routers.DefaultRouter()
+router.register(r'countries', CountryViewSet)
 
 urlpatterns = [
-    url(r'^countries/$', country_list, name='country-view'),
+    url(r'^', include(router.urls)),
     url(r'^countries/get_border_as_geojson/(?P<country>[^0-9]+)/$',
         GetGeoJSONByCountry.as_view()),
     url(r'^countries/get_centroid_as_geojson/(?P<country>[^0-9]+)/$',
         GetCentroidByCountry.as_view())
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+#urlpatterns = format_suffix_patterns(urlpatterns)
